f8a52bc7d936d31eb0412ab0e1d680a115f13db4
BackGwa <backgwa@icloud.com>
2023-04-29 02:23:55 +0900

    창 구현

--- scripts/desktop.js
+++ scripts/desktop.js
@@ -2,9 +2,16 @@ const time = document.getElementById('timedisplay');
 const box = document.getElementById("box");
 const desktop = document.getElementById("desktop");
 
+const window_INFO = document.getElementById("INFO");
+const title_INFO = document.getElementById("INFOTITLE");
+
+
+
 let startPosition;
 let isDragging;
 
+let windowsDrag = false;
+let INFO = false;
 
 const counter = setInterval(function(){
     const today = new Date();
@@ -17,7 +24,14 @@ document.onmousemove = (e) => {
     box.style.left = e.pageX + 'px';
     box.style.top = e.pageY + 'px';
 
-    if (isDragging && e.target.id != 'hintdisable') {
+    if (windowsDrag && INFO && isDragging) {
+        console.log(`${e.pageX}, ${e.pageY}`);
+        window_INFO.style.top = `${e.pageY}px`;
+        window_INFO.style.left = `${e.pageX - 250}px`;
+        window_INFO.style.transform = `translate(-50%, -5%)`
+    }
+
+    if (isDragging && e.target.id != 'hintdisable' && !windowsDrag) {
         let distanceX = e.pageX - startPosition[0];
         let distanceY = e.pageY - startPosition[1];
 
@@ -30,6 +44,12 @@ document.onmousemove = (e) => {
     } else if(isDragging) {
         box.style.opacity = '0.0';
     }
+
+}
+
+title_INFO.onmousedown = () => {
+    windowsDrag = true;
+    INFO = true;
 }
 
 document.onmousedown = (e) => {
@@ -40,6 +60,8 @@ document.onmousedown = (e) => {
 
 document.onmouseup = () => {
     isDragging = false;
+    windowsDrag = false;
+    INFO = false;
     box.style.width = '0px';
     box.style.height = '0px';
     box.style.opacity = '0.0';
