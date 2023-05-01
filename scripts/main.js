34e38c257d9b314197b0aad738672060513f4537
BackGwa <backgwa@icloud.com>
2023-05-02 00:05:25 +0900

    비프음 추가

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,12 @@ document.onclick = (e) => {
     document.querySelector(".message").style.animation = "ereft 0.1s forwards";
     setTimeout(() => {
         document.querySelector(".message").remove();
-    }, 150)
+    }, 150);
     document.getElementById("boot").play();
     setTimeout(() => {
+        document.getElementById("beep").play();
+    }, 4000);
+    setTimeout(() => {
         location.href = './startup.html';
-    }, 13000)
+    }, 9000);
 }
