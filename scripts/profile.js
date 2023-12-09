0bba5b7e558a0acbaa1dc287816176ec5beb2597
BackGwa <backgwa@icloud.com>
2023-12-09 20:50:42 +0900

    날짜 버그 수정

--- scripts/profile.js
+++ scripts/profile.js
@@ -122,13 +122,13 @@ function time_update() {
     const week = ['일', '월', '화', '수', '목', '금', '토'];
     const today = new Date();
     const month = String(today.getMonth()).padStart(2, '0');
-    const day = String(today.getDay()).padStart(2, '0');
+    const date = String(today.getDate()).padStart(2, '0');
     const week_index = today.getDay();
 
     hours = String(today.getHours()).padStart(2, '0');
     minutes = String(today.getMinutes()).padStart(2, '0');
 
-    time_label.innerHTML = `${month}월 ${day}일 (${week[week_index]}) ${hours}:${minutes}`;
+    time_label.innerHTML = `${month}월 ${date}일 (${week[week_index]}) ${hours}:${minutes}`;
 }
 
 // 전체호면 스크린샷
