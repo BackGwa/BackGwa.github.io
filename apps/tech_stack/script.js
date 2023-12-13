ec8fdd6c2485e5b2906aff3844b992d03aecb987
BackGwa <backgwa@icloud.com>
2023-12-14 08:40:40 +0900

    Ctrl 키를 누르면 줌되는 기능 추가

---
+++ apps/tech_stack/script.js
@@ -0,0 +1,31 @@
+
+let move_scale;
+
+window.onload = () => {
+    move_scale = document.querySelector(".move-scale");
+
+    move_scale.addEventListener("wheel", (e) => {
+        if (e.ctrlKey) {
+            transform_value = move_scale.style.transform
+            scale_value = transform_value.match(/scale\(([^)]+)\)/);
+            scale = parseFloat(scale_value[1]);
+
+            if (e.deltaY > 0 && scale > 0.1) {
+                scale = scale - 0.1;
+            } else {
+                scale = scale + 0.1;
+            }
+            move_scale.style.transform = `scale(${scale})`
+        }
+    });
+
+    addEventListener("keydown", (e) => {
+        if (e.ctrlKey) {
+            document.querySelector(".move-layout").style.overflow = "hidden";
+        }
+    });
+
+    addEventListener("keyup", () => {
+        document.querySelector(".move-layout").style.overflow = "hidden";
+    });
+}
