457186546ea987435c97e4789b6e53d5616b973b
BackGwa <backgwa@icloud.com>
2023-12-14 09:56:51 +0900

    피그마 디자인 업데이트

--- apps/tech_stack/script.js
+++ apps/tech_stack/script.js
@@ -6,26 +6,21 @@ window.onload = () => {
 
     move_scale.addEventListener("wheel", (e) => {
         if (e.ctrlKey) {
+            document.querySelector(".move-layout").style.overflow = "hidden";
             transform_value = move_scale.style.transform
             scale_value = transform_value.match(/scale\(([^)]+)\)/);
             scale = parseFloat(scale_value[1]);
 
             if (e.deltaY > 0 && scale > 0.1) {
                 scale = scale - 0.1;
-            } else {
+            } else if (e.deltaY < 0 && scale > 0.1) {
                 scale = scale + 0.1;
+            } else {
+                document.querySelector(".move-layout").style.overflow = "scroll";
             }
-            move_scale.style.transform = `scale(${scale})`
+            move_scale.style.transform = `scale(${scale})`;
+        } else {
+            document.querySelector(".move-layout").style.overflow = "scroll";
         }
-    });
-
-    addEventListener("keydown", (e) => {
-        if (e.ctrlKey) {
-            document.querySelector(".move-layout").style.overflow = "hidden";
-        }
-    });
-
-    addEventListener("keyup", () => {
-        document.querySelector(".move-layout").style.overflow = "hidden";
     });
 }
