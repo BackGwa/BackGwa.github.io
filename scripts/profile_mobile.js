8940994cc7750a0ec2ffed268028155a1bf2a095
BackGwa <backgwa@icloud.com>
2024-01-08 12:23:46 +0900

    앱 열기/닫기 애니메이션 추가

--- scripts/profile_mobile.js
+++ scripts/profile_mobile.js
@@ -1,8 +1,11 @@
+let app_area;
+
 function page_init() {
     setTimeout(() => {
         play("../res/sounds/iPhone_Startup.mp3");
     }, 250);
 
+    app_area = document.querySelector(".app-area");
     time_label = document.querySelector(".time-label");
     mobile_time_register();
     event_register();
@@ -10,4 +13,12 @@ function page_init() {
 
 function event_register() {
 
+}
+
+function show_app(app_name) {
+    app_area.classList.add("show-app");
+}
+
+function unshow_app() {
+    app_area.classList.remove("show-app");
 }
