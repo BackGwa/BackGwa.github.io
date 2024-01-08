c3abd408fa59d96a21fcfb7a0072b3e8db529250
BackGwa <backgwa@icloud.com>
2024-01-08 12:06:42 +0900

    모바일 페이지 독 및 상단 라벨 추가

--- scripts/profile_mobile.js
+++ scripts/profile_mobile.js
@@ -1,5 +1,13 @@
 function page_init() {
     setTimeout(() => {
         play("../res/sounds/iPhone_Startup.mp3");
-    }, 500);
+    }, 250);
+
+    time_label = document.querySelector(".time-label");
+    mobile_time_register();
+    event_register();
+}
+
+function event_register() {
+
 }
